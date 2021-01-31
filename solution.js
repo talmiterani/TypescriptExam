3e9036c06ab21f1eb456debbebdf7ae64421ddb5
tal miterani <tal.miterani@gmail.com>
2021-01-31 11:00:01 +0200

    SOLUTION FOR EXAM IS READY

--- solution.js
+++ solution.js
@@ -1,13 +1,8 @@
-console.log("STRATING PROJECT");
-// let text = (<HTMLInputElement>document.querySelector("#todo-item")).value;
 var text = document.querySelector("#todo-item");
-// HTMLElement;let textId:any = document.getElementById("#todo-item");
 HTMLElement;
 var todoSave = document.querySelector("#todo-save");
 HTMLElement;
 var todoList = document.querySelector("#todo-list");
-// let text=textId.value;
-// let saveTask = todoSave
 var tasksArr = [];
 var count;
 var Task = /** @class */ (function () {
@@ -38,31 +33,37 @@ var Task = /** @class */ (function () {
     return Task;
 }());
 todoSave.addEventListener("click", function () {
-    console.log(text.value);
+    if (text.value == '') {
+        return;
+    }
+    updateArrId();
     var newTask = new Task(count, text.value);
     tasksArr.push(newTask);
     localStorage.setItem("Obj", JSON.stringify(tasksArr));
-    location.reload();
 });
-// function clearAll() {
-//     text = '';
-// }
+function deleteAll() {
+    var items = JSON.parse(localStorage.getItem("Obj"));
+    items = [];
+    for (var i = 0; i < todoList.children.length; i++) {
+        todoList.children[i].remove();
+        i--;
+    }
+    items = JSON.stringify(items);
+    localStorage.setItem("Obj", items);
+    location.reload();
+}
 window.onload = function () {
-    // clearAll();
+    updateArrId();
     if (JSON.parse(localStorage.getItem("Obj"))) {
         tasksArr = JSON.parse(localStorage.getItem("Obj"));
-        //console.log(notesArr)
         count = tasksArr.length;
-        console.log(tasksArr);
         for (var _i = 0, tasksArr_1 = tasksArr; _i < tasksArr_1.length; _i++) {
             var task = tasksArr_1[_i];
             if (task != null) {
                 var div = document.createElement("div");
                 div.classList.add("todo-row");
-                console.log(text);
-                div.innerHTML = "<span id='" + task["_id"] + "' class='todo-item'>" + task["_txt"] + "</span><button class='todo-ok' onclick='done(" + task["_id"] + ")'><i class=\"fas fa-check text-success\"></i></button>";
+                div.innerHTML = "<span id='" + task["_id"] + "' class='todo-item'>" + task["_txt"] + "</span><button class='todo-ok cx' onclick='done(" + task["_id"] + ")'><i class=\"fas fa-check text-success\"></i></button>";
                 todoList.append(div);
-                console.log(todoList);
             }
         }
     }
@@ -71,16 +72,38 @@ window.onload = function () {
     }
 };
 function done(key) {
-    console.log(todoList.children);
-    for (var i = 0; i < todoList.length; i++) {
-        if (todoList[i] != null) {
-            if (todoList[i]["_id"] == key) {
-                console.log(todoList[i]);
-                break;
+    var spanArr = document.getElementsByTagName('span');
+    for (var i = 0; i < todoList.children.length; i++) {
+        if (todoList.children[i].children[0].id == key) {
+            todoList.children[i].children[0].classList.add('done');
+            break;
+        }
+    }
+    console.log(spanArr);
+}
+function deleteCompleted() {
+    var items = JSON.parse(localStorage.getItem("Obj"));
+    for (var i = 0; i < todoList.children.length; i++) {
+        if (todoList.children[i] != null) {
+            if (todoList.children[i].children[0].classList.contains("done")) {
+                todoList.children[i].remove();
+                items.splice(i, 1);
+                i--;
             }
         }
     }
-    // localStorage.removeItem("Obj")
-    // localStorage.setItem("Obj", JSON.stringify(notesArr))
-    // location.reload()
+    items = JSON.stringify(items);
+    localStorage.setItem("Obj", items);
+    location.reload();
+}
+function updateArrId() {
+    for (var i = 0; i < count; i++) {
+        todoList.children[i].children[0].id = i;
+    }
+    var tempArr = JSON.parse(localStorage.getItem("Obj"));
+    for (var i = 0; i < tempArr.length; i++) {
+        tempArr[i]._id = i;
+    }
+    tempArr = JSON.stringify(tempArr);
+    localStorage.setItem("Obj", tempArr);
 }
--- solution.ts
+++ solution.ts
@@ -1,113 +1,111 @@
-console.log("STRATING PROJECT");
 
-// let text = (<HTMLInputElement>document.querySelector("#todo-item")).value;
 let text: HTMLInputElement = document.querySelector("#todo-item")
-
-// HTMLElement;let textId:any = document.getElementById("#todo-item");
 HTMLElement;let todoSave:any = document.querySelector("#todo-save");
 HTMLElement;let todoList:any = document.querySelector("#todo-list");
-
-// let text=textId.value;
-// let saveTask = todoSave
-
 let tasksArr: Array<Task> = []
 let count:number;
-
-
 class Task {
-
     private _id: number
-    private _txt: string
- 
-  
-      constructor(id: number, txt: string) {
-          this._id = id
-          this._txt = txt;
-        }
-  
-        get id(): number {
-          return this._id;
-        }
-  
-        set id(newId: number) {
-          this._id = newId;
-        }
-  
-        get txt(): string {
-          return this._txt;
-        }
-  
-        set txt(newTxt: string) {
-          this._txt = newTxt;
-        }
-  
-  
-  }
-
+    private _txt: string 
+    constructor(id: number, txt: string) {
+        this._id = id
+        this._txt = txt;
+    }
+    get id(): number {
+        return this._id;
+    }
+    set id(newId: number) {
+        this._id = newId;
+    }
+    get txt(): string {
+        return this._txt;
+    }
+    set txt(newTxt: string) {          
+        this._txt = newTxt;
+    }
+}
 todoSave.addEventListener("click", ():void => {
-    console.log(text.value)
+    if(text.value==''){
+        return;
+    }
+    updateArrId()
     let newTask = new Task(count,text.value);
-
     tasksArr.push(newTask)
-
     localStorage.setItem("Obj", JSON.stringify(tasksArr))
-
-    location.reload()
 });
+function deleteAll() {
 
+    let items = JSON.parse(localStorage.getItem("Obj"));
+    items = [];
+    for (let i = 0 ; i < todoList.children.length ; i++) {
+            todoList.children[i].remove();
+            i--;
+        
+    }
+    items = JSON.stringify(items);
+    localStorage.setItem("Obj", items);
+    location.reload()
 
-// function clearAll() {
-
-//     text = '';
-
-// }
+}
   window.onload = (): void => {
-
-    // clearAll();
-
+    updateArrId()
     if (JSON.parse(localStorage.getItem("Obj"))) {
 
         tasksArr = JSON.parse(localStorage.getItem("Obj"))
-        //console.log(notesArr)
-        count = tasksArr.length
-        console.log(tasksArr)
-        for (let task of tasksArr) {
+        count = tasksArr.length;
+        for(let task of tasksArr) {
 
           if(task != null) {
 
-              let div = document.createElement("div")
+            let div = document.createElement("div")
 
             div.classList.add("todo-row");
-            console.log(text);
-            div.innerHTML = `<span id='${task["_id"]}' class='todo-item'>${task["_txt"]}</span><button class='todo-ok' onclick='done(${task["_id"]})'><i class="fas fa-check text-success"></i></button>`
+            div.innerHTML = `<span id='${task["_id"]}' class='todo-item'>${task["_txt"]}</span><button class='todo-ok cx' onclick='done(${task["_id"]})'><i class="fas fa-check text-success"></i></button>`
 
             todoList.append(div)
-            console.log(todoList);
           }
         }
     }
-
     else {
       count = 0
     }
 }
 
 function done(key: number): void {
-
-    console.log(todoList.children);
-
-    // for (let i = 0 ; i < todoList.length ; i++) {
-  
-    //   if (todoList[i] != null) {
-    //     if (todoList[i]["_id"] == key) {
-    //         console.log(todoList[i]);
-    //       break
-    //     }
-    //   }
-    // }
-  
-    // localStorage.removeItem("Obj")
-    // localStorage.setItem("Obj", JSON.stringify(notesArr))
-  
-    // location.reload()
+    let spanArr = document.getElementsByTagName('span');
+    for (let i = 0 ; i < todoList.children.length ; i++) {
+        if (todoList.children[i].children[0].id == key) {
+            todoList.children[i].children[0].classList.add('done');
+            break;
+        }
+    }
+    console.log(spanArr);
   }
+function deleteCompleted ():void {
+    var items = JSON.parse(localStorage.getItem("Obj"));
+    for (let i = 0 ; i < todoList.children.length ; i++) {
+  
+        if (todoList.children[i] != null) {
+
+            if (todoList.children[i].children[0].classList.contains("done")) {
+                todoList.children[i].remove();
+                items.splice(i, 1);
+                i--;
+            }
+        }
+    }    
+    items = JSON.stringify(items);
+    localStorage.setItem("Obj", items);
+    location.reload()
+}
+function updateArrId():void{
+    for(let i=0;i<count;i++){
+        todoList.children[i].children[0].id = i;
+    }
+    let tempArr = JSON.parse(localStorage.getItem("Obj"));
+    for(let i=0;i<tempArr.length;i++){
+        tempArr[i]._id = i;
+    }
+    tempArr = JSON.stringify(tempArr);
+    localStorage.setItem("Obj", tempArr);
+}
